efa683522808447664c1d99c31827942336d3bed
z5651876 <z5651876@vx22.orchestra.cse.unsw.EDU.AU>
2025-09-11 21:56:55 +1000

    stub(iter0): add adminAuthRegister returning { controlUserId: 1 }

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,12 @@
 // This file should contain your functions relating to:
 // - adminAuth*
 // - adminControlUser*
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+  return {
+    controlUserId: 1,
+  };
+}
+
+module.exports = {
+  adminAuthRegister,
+};
