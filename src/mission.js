2f9b09d65befa075abd742af9f67e37788846c8d
Zhennan Chen <z5521425@ad.unsw.edu.au>
2025-09-12 23:08:51 +1000

    Update file mission.js

--- src/mission.js
+++ src/mission.js
@@ -1,2 +1,51 @@
 // This file should contain your functions relating to:
 // - adminMission*
+function adminMissionList(controlUserId) {
+    return {
+      missions: [
+        {
+          missionId: 1,
+          name: "Mercury",
+        },
+        {
+          missionId: 2,
+          name: "Apollo",
+        },
+      ],
+    };
+  }
+  function adminMissionRemove(controlUserId, missionId) {
+    return {};
+  }
+  
+  
+  function adminMissionCreate(controlUserId, name, description, target ) {
+      
+    return {
+      missionId: 2
+    };
+  }
+  function adminMissionInfo(controlUserId, missionId) {
+    return { 
+    missionId: 1,
+    name: 'Mercury',
+    timeCreated: 1683125870,
+    timeLastEdited: 1683125871,
+    description: "Place a manned spacecraft in orbital flight around the earth. Investigate a persons performance capabilities and their ability to function in the environment of space. Recover the person and the spacecraft safely",
+    target: 'Earth orbit'
+  
+    }
+  }
+  function adminMissionNameUpdate(controlUserId, missionId, name) {
+    return {}
+  }
+  
+  // Update mission target
+  function adminMissionTargetUpdate(controlUserId, missionId, target) {
+      return {};
+    }
+
+  // Update mission description
+  function adminMissionDescriptionUpdate(controlUserId, missionId, description) {
+      return {};
+    }
