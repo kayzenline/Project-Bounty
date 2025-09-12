846bfe325c39b7f3a691e369d14b1366a0cfa4eb
Qinglin Ma <z56533087@ad.unsw.edu.au>
2025-09-12 22:22:23 +1000

    add adminMissionInfo to mission.js

--- src/mission.js
+++ src/mission.js
@@ -25,3 +25,14 @@ function adminMissionCreate(controlUserId, name, description, target ) {
     missionId: 2
   };
 }
+function adminMissionInfo(controlUserId, missionId) {
+  return { 
+  missionId: 1,
+  name: 'Mercury',
+  timeCreated: 1683125870,
+  timeLastEdited: 1683125871,
+  description: "Place a manned spacecraft in orbital flight around the earth. Investigate a persons performance capabilities and their ability to function in the environment of space. Recover the person and the spacecraft safely",
+  target: 'Earth orbit'
+
+  }
+}
