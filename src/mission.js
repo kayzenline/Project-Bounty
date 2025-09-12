a19a18973f862d17d298d401bb3a296782e7d3ed
Jiawen LIN <z5647814@ad.unsw.edu.au>
2025-09-12 11:00:44 +1000

    Add stub for adminMissionCreate

--- src/mission.js
+++ src/mission.js
@@ -1,2 +1,8 @@
 // This file should contain your functions relating to:
 // - adminMission*
+  function adminMissionCreate(controlUserId, name, description, target ) {
+    
+    return {
+      missionId: 2
+    };
+  }
