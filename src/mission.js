6812753a5ccac6e5e7a7e3baf0ac62b24e6a8940
Jiawen LIN <z5647814@ad.unsw.edu.au>
2025-09-12 11:07:04 +1000

    Add stub for adminMissionList

--- src/mission.js
+++ src/mission.js
@@ -1,2 +1,16 @@
 // This file should contain your functions relating to:
 // - adminMission*
+function adminMissionList(controlUserId) {
+  return {
+    missions: [
+      {
+        missionId: 1,
+        name: "Mercury",
+      },
+      {
+        missionId: 2,
+        name: "Apollo",
+      },
+    ],
+  };
+}
