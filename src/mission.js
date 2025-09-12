3953d5435bd68a2869b5cdf87c358079df53ed31
Zhennan Chen <z5521425@ad.unsw.edu.au>
2025-09-12 22:47:56 +1000

    Update file mission.js

--- src/mission.js
+++ src/mission.js
@@ -1,2 +1,6 @@
 // This file should contain your functions relating to:
 // - adminMission*
+// Update mission target
+function adminMissionTargetUpdate(controlUserId, missionId, target) {
+    return {};
+  }
