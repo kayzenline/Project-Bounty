27da5deed70ec77259d4d90df2b1171f6b91c4ad
Xinyuan Li <z5651876@ad.unsw.edu.au>
2025-09-24 11:13:02 +1000

    Implement adminControlUserPasswordUpdate with tests

---
+++ src/tests/adminControlUserPasswordUpdate.test.js
@@ -0,0 +1,50 @@
+import {
+  adminAuthRegister,
+  adminAuthLogin,
+  adminControlUserPasswordUpdate,
+} from '../auth.js';
+import { clear } from '../other.js';
+
+beforeEach(() => clear());
+
+describe('adminControlUserPasswordUpdate (minimal spec set)', () => {
+  test('happy path: change -> old fails, new works', () => {
+    const { controlUserId } = adminAuthRegister('pw@x.com', 'Oldpass123', 'Amy', 'Pond');
+
+    expect(adminControlUserPasswordUpdate(controlUserId, 'Oldpass123', 'Newpass123'))
+      .toEqual({});
+
+    expect(adminAuthLogin('pw@x.com', 'Oldpass123').errorCategory).toBe('BAD_INPUT');
+    expect(adminAuthLogin('pw@x.com', 'Newpass123')).toEqual({ controlUserId });
+  });
+
+  test('INVALID_CREDENTIALS: user not found', () => {
+    const res = adminControlUserPasswordUpdate(999999, 'a', 'b');
+    expect(res.errorCategory).toBe('INVALID_CREDENTIALS');
+  });
+
+  test('BAD_INPUT: wrong old password', () => {
+    const { controlUserId } = adminAuthRegister('a@b.com', 'Abcd1234', 'A', 'B');
+    const res = adminControlUserPasswordUpdate(controlUserId, 'WRONG', 'Newpass123');
+    expect(res.errorCategory).toBe('BAD_INPUT');
+  });
+
+  test('BAD_INPUT: old === new', () => {
+    const { controlUserId } = adminAuthRegister('c@d.com', 'Abcd1234', 'C', 'D');
+    const res = adminControlUserPasswordUpdate(controlUserId, 'Abcd1234', 'Abcd1234');
+    expect(res.errorCategory).toBe('BAD_INPUT');
+  });
+
+  test('BAD_INPUT: new password too weak (representative)', () => {
+    const { controlUserId } = adminAuthRegister('e@f.com', 'Abcd1234', 'E', 'F');
+    const res = adminControlUserPasswordUpdate(controlUserId, 'Abcd1234', 'OnlyLetters');
+    expect(res.errorCategory).toBe('BAD_INPUT');
+  });
+
+  test('BAD_INPUT: cannot reuse any previous password', () => {
+    const { controlUserId } = adminAuthRegister('g@h.com', 'Abcd1234', 'G', 'H');
+    expect(adminControlUserPasswordUpdate(controlUserId, 'Abcd1234', 'Qwerty123')).toEqual({});
+    const res = adminControlUserPasswordUpdate(controlUserId, 'Qwerty123', 'Abcd1234');
+    expect(res.errorCategory).toBe('BAD_INPUT');
+  });
+});
