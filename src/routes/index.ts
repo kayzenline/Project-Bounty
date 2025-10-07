83eb4c0a0cd9631a6601070e0bbe4c950a9864c4
Jiawen <z5647814@ad.unsw.edu.au>
2025-10-07 19:29:44 +1100

    feat: create stubs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,10 @@
-import { Router } from "express";
+import { Router } from 'express';
+import v1Router from './v1';
+import { notImplementedHandler } from './utils';
 
 const router = Router();
+
+router.use('/v1', v1Router);
+router.delete('/clear', notImplementedHandler);
 
 export default router;
