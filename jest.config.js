5f4a5c1abed51802bc9c176b1083ac42accb84c1
Jiawen LIN <z5647814@ad.unsw.edu.au>
2025-10-10 23:25:29 +1100

    fix: skip tests/tests in ci

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 // This file is used to configure jest
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    maxWorkers: 1,
-    verbose: true,
-    transform: {
-      '^.+\\.(ts|tsx|js)$': 'ts-jest'
-    },
-    testPathIgnorePatterns: ['/tests/tests/'] 
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  maxWorkers: 1,
+  verbose: true,
+  transform: {
+    '^.+\\.(ts|tsx|js)$': 'ts-jest'
+  },
+  testMatch: ['**/tests/http/**/*.test.ts']
+};
