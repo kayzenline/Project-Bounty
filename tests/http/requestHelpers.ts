54c73380946890b69b97e7a457fabc30ff809a47
Jiawen LIN <z5647814@ad.unsw.edu.au>
2025-10-10 23:25:52 +1100

    feat: add type for example

--- tests/http/requestHelpers.ts
+++ tests/http/requestHelpers.ts
@@ -1,19 +1,17 @@
 import request from 'sync-request-curl';
-const port = config.port;
-const url = config.url;
+import config from '../../src/config.json';
 
+const { port, url } = config;
 const SERVER_URL = `${url}:${port}`;
 
-
-export function adminMissionTransferRequest(controlUserSessionId, missiondId, userEmail) {
-    let response = request(
-        'POST',
-        `${SERVER_URL}/v1/admin/mission/${missiondId}/tranfer`,
-        {
-            json : {userEmail},
-            headers: {controlUserSessionId}
-        }
-    );
-
-    return response;
+export function adminMissionTransferRequest(
+  controlUserSessionId: string,
+  missionId: number,
+  userEmail: string
+) {
+  let response = request('POST', `${SERVER_URL}/v1/admin/mission/${missionId}/transfer`, {
+    json: { userEmail },
+    headers: { controlusersessionid: controlUserSessionId }
+  });
+  return response;
 }
