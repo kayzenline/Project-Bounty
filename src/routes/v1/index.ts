86dfc03704e8fa75aa33b317cebc63149d412011
Xinyuan Li <z5651876@ad.unsw.edu.au>
2025-10-15 16:10:06 +1100

    fix: remove trailing spaces in v1 index.ts

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import adminRouter from './admin';
-import clearRouter from './clear'; 
+import clearRouter from './clear';
 
 const router = Router();
 
 router.use('/admin', adminRouter);
-router.use('/clear', clearRouter);  
+router.use('/clear', clearRouter);
 
 export default router;
