54d1cd011aafb055dc446506f631828272b55bc8
Xinyuan Li <z5651876@ad.unsw.edu.au>
2025-10-15 15:55:25 +1100

    Implement and pass all tests for clear endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express';
 import v1Router from './v1';
-import { clearHandler } from '../other';
 const router = Router();
 
 router.use('/v1', v1Router);
 // Align clear route with README/spec: provide DELETE /clear and /clear/v1
-router.delete('/clear', clearHandler);
-/* export function clearHandler(_req: Request, res: Response) {
-  clear();
-  return res.status(200).json({});
-} */
-// not sure should it be here
+
 export default router;
