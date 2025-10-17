e4dd5ea702f0ee0258a4da4869ad07e5eb0bae3c
Xinyuan Li <z5651876@ad.unsw.edu.au>
2025-10-17 17:31:42 +1100

    style: (fix lint)

--- src/routes/v1/clear.ts
+++ src/routes/v1/clear.ts
@@ -1,12 +1,12 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import { clear } from '../../other'; 
+import { clear } from '../../other';
 
 const router = Router();
 
 router.delete('/', (_req: Request, res: Response, next: NextFunction) => {
   try {
     const result = clear();
-    return res.status(200).json(result); 
+    return res.status(200).json(result);
   } catch (err) {
     return next(err);
   }
