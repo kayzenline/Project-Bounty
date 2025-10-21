a41ad55c7069c33917d7d453fa5e498b91f57f88
zhennanchen <z5521425@ad.unsw.edu.au>
2025-10-21 21:42:50 +1100

    adminMissionDescriptionUpdate.test.ts

--- tests/http/adminMissionDescriptionUpdate.test.ts
+++ tests/http/adminMissionDescriptionUpdate.test.ts
@@ -1,9 +1,7 @@
 import { v4 as uuid } from 'uuid';
-import { adminMissionCreate, adminMissionInfo } from '../../src/mission';
-import { adminAuthRegister, adminAuthLogin } from '../../src/auth';
+import { adminMissionInfo } from '../../src/mission';
 import { findSessionFromSessionId, generateSessionId } from '../../src/helper';
 import { missionDescriptionUpdate, clearRequest, controlUserSessionId as missionCreate, userRegister, userLogin } from './requestHelpers';
-import { getData } from '../../src/dataStore';
 
 function uniqueEmail(prefix = 'user') {
   return `${prefix}.${uuid()}@example.com`;
@@ -40,7 +38,7 @@ describe('HTTP tests for MissionDescriptionUpdate', () => {
       const controlUserId = session.controlUserId;
       const newDescription = 'Land humans on the Moon and bring them safely back to Earth';
       const res = missionDescriptionUpdate(controlUserSessionId, missionId, newDescription);
-      const resultBody = res.body
+      const resultBody = res.body;
       expect(res.statusCode).toBe(200);
       expect(resultBody).toBe({});
       const updatedDesc = adminMissionInfo(controlUserId, missionId).description;
@@ -53,7 +51,7 @@ describe('HTTP tests for MissionDescriptionUpdate', () => {
     if (session) {
       const longDescription = 'J'.repeat(401);
       const res = missionDescriptionUpdate(controlUserSessionId, missionId, longDescription);
-      const resultBody = res.body
+      const resultBody = res.body;
       expect(res.statusCode).toBe(400);
       expect(resultBody).toEqual({ error: expect.any(String) });
     }
@@ -68,7 +66,7 @@ describe('HTTP tests for MissionDescriptionUpdate', () => {
     expect(resultBody).toEqual({ error: expect.any(String) });
 
     const res1 = missionDescriptionUpdate('', missionId, newDescription);
-    const resultBody1 = res1.body
+    const resultBody1 = res1.body;
     expect(res1.statusCode).toBe(401);
     expect(resultBody1).toEqual({ error: expect.any(String) });
   });
@@ -89,21 +87,17 @@ describe('HTTP tests for MissionDescriptionUpdate', () => {
     const newRes = missionCreate(newSessionId, newMission.name, newMission.description, newMission.target);
     expect(newRes.statusCode).toBe(200);
     const newMissionId = newRes.body.missionId;
+    console.log('the new session id:', newSessionId);
 
     const newDescription = 'Land humans on the Moon and bring them safely back to Earth';
-
     const res = missionDescriptionUpdate(newSessionId, missionId, newDescription);
-    const resultBody = res.body
+    const resultBody = res.body;
     expect(res.statusCode).toBe(403);
     expect(resultBody).toEqual({ error: expect.any(String) });
 
-    const session = findSessionFromSessionId(controlUserSessionId);
-    if (session) {
-      const newDescription = 'Land humans on the Moon and bring them safely back to Earth';
-      const res = missionDescriptionUpdate(controlUserSessionId, newMissionId + 1, newDescription);
-      const resultBody = res.body
-      expect(res.statusCode).toBe(403);
-      expect(resultBody).toEqual({ error: expect.any(String) });
-    }
+    const res1 = missionDescriptionUpdate(controlUserSessionId, newMissionId + 1, newDescription);
+    const resultBody1 = res1.body;
+    expect(res1.statusCode).toBe(403);
+    expect(resultBody1).toEqual({ error: expect.any(String) });
   });
 });
