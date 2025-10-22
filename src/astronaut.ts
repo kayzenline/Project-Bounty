f036d386ef19e56a28b95984ace6387a0d30bc0f
zhennanchen <z5521425@ad.unsw.edu.au>
2025-10-23 09:41:09 +1100

    astronaut.ts

---
+++ src/astronaut.ts
@@ -0,0 +1,66 @@
+import { getData, setData } from './dataStore';
+import {
+  controlUserIdCheck,
+  missionIdCheck,
+  findSessionFromSessionId,
+  astronautIdCheck,
+  astronautNameCheck,
+  astronautRankCheck,
+  astronautPhyCharCheck,
+  normalizeError,
+  ServiceError
+} from './helper';
+import { errorCategories as EC } from './testSamples';
+
+function seeAstronautPool(controlUserSessionId: string) {
+
+}
+
+function createAstronaut(
+  controlUserSessionId: string,
+  nameFirst: string,
+  nameLast: string,
+  rank: string,
+  age: number,
+  weight: number,
+  height: number
+) {
+
+}
+
+function deleteAstronaut(controlSessionUserId: string, astronautId: number) {
+
+}
+
+function getAstronautInfo(controlSessionUserId: string, astronautId: number) {
+
+}
+
+function editAstronaut(
+  controlUserSessionId: string,
+  astronautId: number,
+  nameFirst: string,
+  nameLast: string,
+  rank: string,
+  age: number,
+  weight: number,
+  height: number
+) {
+
+}
+
+function assginAstronaut(
+  controlUserSessionId: string,
+  astronautId: number,
+  missionId: number
+) {
+
+}
+
+function unassginAstronaut(
+  controlUserSessionId: string,
+  astronautId: number,
+  missionId: number
+) {
+
+}
