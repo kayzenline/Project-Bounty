1ae092f9398301f3af39e8e67d9ddfd860508b38
zhennanchen <z5521425@ad.unsw.edu.au>
2025-10-23 11:36:39 +1100

    astronautFix

--- src/routes/v1/admin/astronaut.ts
+++ src/routes/v1/admin/astronaut.ts
@@ -1,46 +1,45 @@
-/*
+
 import { Router } from 'express';
 import { notImplementedHandler } from '../../utils';
-import { seeAstronautPool, createAstronaut, deleteAstronaut, getAstronautInfo, editAstronaut, assginAstronaut, unassginAstronaut } from '../../../astronaut';
-import { httpToErrorCategories } from '../../../testSamples';
+// import { deleteAstronaut, editAstronaut, seeAstronautPool, createAstronaut, getAstronautInfo, assginAstronaut, unassginAstronaut } from '../../../astronaut';
+// import { httpToErrorCategories } from '../../../testSamples';
 
 const router = Router();
 
 router.get('/pool', notImplementedHandler);
 router.post('/', );
 router.get('/:astronautid', notImplementedHandler);
-router.put('/:astronautid', (req, res) => {
-  const controlUserSessionId = req.header('controlUserSessionId');
-  const astronautId = Number(req.params.astronautid);
-  const { nameFirst, nameLast, rank, age, weight, height } = req.body || {};
+// router.put('/:astronautid', (req, res) => {
+//   const controlUserSessionId = req.header('controlUserSessionId');
+//   const astronautId = Number(req.params.astronautid);
+//   const { nameFirst, nameLast, rank, age, weight, height } = req.body || {};
 
-  const result = editAstronaut(
-    controlUserSessionId,
-    astronautId,
-    nameFirst,
-    nameLast,
-    rank,
-    age,
-    weight,
-    height
-  );
-  if ('error' in result) {
-    const status = httpToErrorCategories[result.errorCategory as keyof typeof httpToErrorCategories];
-    return res.status(status).json({ error: result.error });
-  }
-  return res.status(200).json({});
-});
-router.delete('/:astronautid', (req, res) => {
-  const controlUserSessionId = req.header('controlUserSessionId');
-  const astronautId = Number(req.params.astronautid);
+//   const result = editAstronaut(
+//     controlUserSessionId,
+//     astronautId,
+//     nameFirst,
+//     nameLast,
+//     rank,
+//     age,
+//     weight,
+//     height
+//   );
+//   if ('error' in result) {
+//     const status = httpToErrorCategories[result.errorCategory as keyof typeof httpToErrorCategories];
+//     return res.status(status).json({ error: result.error });
+//   }
+//   return res.status(200).json({});
+// });
+// router.delete('/:astronautid', (req, res) => {
+//   const controlUserSessionId = req.header('controlUserSessionId');
+//   const astronautId = Number(req.params.astronautid);
 
-  const result = deleteAstronaut(controlUserSessionId, astronautId);
-  if ('error' in result) {
-    const status = httpToErrorCategories[result.errorCategory as keyof typeof httpToErrorCategories];
-    return res.status(status).json({ error: result.error });
-  }
-  return res.status(200).json({});
-});
+//   const result = deleteAstronaut(controlUserSessionId, astronautId);
+//   if ('error' in result) {
+//     const status = httpToErrorCategories[result.errorCategory as keyof typeof httpToErrorCategories];
+//     return res.status(status).json({ error: result.error });
+//   }
+//   return res.status(200).json({});
+// });
 
 export default router;
-*/
