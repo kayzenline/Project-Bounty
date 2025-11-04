8f9106ebfc7ef2ce0c3fef7b7e99a3540d4b2c56
zhennanchen <z5521425@ad.unsw.edu.au>
2025-11-04 16:48:14 +1100

    httpTests

---
+++ tests/http/adminLaunchVehicleRetire.test.ts
@@ -0,0 +1,15 @@
+import { v4 as uuid } from 'uuid';
+// improte your request here!
+import { getData } from '../../src/dataStore';
+import { generateSessionId } from '../../src/helper';
+
+function uniqueEmail(prefix = 'user') {
+  return `${prefix}.${uuid().split('-').pop() || ''}@example.com`;
+}
+
+describe.skip('Need to write a description', () => {
+  // some helpful functions you may use!
+  uniqueEmail();
+  getData();
+  generateSessionId();
+});
