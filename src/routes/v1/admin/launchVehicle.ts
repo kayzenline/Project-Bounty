44e966c8eecda2d3e243e7e07529726140d4572b
Jiawen <z5647814@ad.unsw.edu.au>
2025-11-07 21:10:29 +1100

    feat: router of launch vehicle detail

---
+++ src/routes/v1/admin/launchVehicle.ts
@@ -0,0 +1,29 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import HTTPError from 'http-errors';
+import { getData } from '../../../dataStore';
+import { adminLaunchVehicleDetails } from '../../../logic/launchVehicle';
+
+
+const router = Router();
+
+router.get('/list', (req: Request, res: Response, next: NextFunction) => {
+  const controlUserSessionId = req.header('controlUserSessionId');
+  try {
+    if (!controlUserSessionId) {
+      throw HTTPError(401, 'ControlUserSessionId is empty or invalid');
+    }
+    const data = getData();
+    const session = data.sessions.find(s => s.controlUserSessionId === controlUserSessionId);
+    if (!session) {
+      throw HTTPError(401, 'ControlUserSessionId is empty or invalid');
+    }
+    const result = adminLaunchVehicleDetails(controlUserSessionId);
+    return res.status(200).json(result);
+  } catch (e) {
+    next();
+  }
+})
+
+
+
+export default router;
