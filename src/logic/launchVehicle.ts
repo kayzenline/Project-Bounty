531302fc0ba28cc957d04dac3dc8a32a966a0942
Jiawen <z5647814@ad.unsw.edu.au>
2025-11-09 14:04:18 +1100

    feat: logic function of adminLaunchVehicleInfo

--- src/logic/launchVehicle.ts
+++ src/logic/launchVehicle.ts
@@ -1,4 +1,6 @@
 import HTTPError from 'http-errors';
+import { launchVehicleIdCheck, controlUserSessionIdCheck, normalizeError, throwErrorForFunction } from './helper';
+import { getData, LaunchVehicleInfo } from '../dataStore';
 
 function notImplemented(): never {
   throw HTTPError(501, 'Not implemented');
@@ -24,8 +26,36 @@ export function adminLaunchVehicleDetails(controlUserSessionId: string) {
 export function adminLaunchVehicleInfo(
   controlUserSessionId: string,
   launchVehicleId: number
-) {
-  return notImplemented();
+): LaunchVehicleInfo {
+  try {
+    controlUserSessionIdCheck(controlUserSessionId);
+    const vehicle = launchVehicleIdCheck(launchVehicleId);
+
+    const launches = getData().launches
+      .filter(l => l.assignedLaunchVehicleId === launchVehicleId)
+      .map(l => ({
+        launch: `[${l.missionCopy.target}] ${l.missionCopy.name} - ${l.createdAt}`,
+        state: l.state
+      }));
+
+    return {
+      launchVehicleId: vehicle.launchVehicleId,
+      name: vehicle.name,
+      description: vehicle.description,
+      maxCrewWeight: vehicle.maxCrewWeight,
+      maxPayloadWeight: vehicle.maxPayloadWeight,
+      launchVehicleWeight: vehicle.launchVehicleWeight,
+      thrustCapacity: vehicle.thrustCapacity,
+      startingManeuveringFuel: vehicle.maneauveringFuel,
+      retired: vehicle.retired,
+      timeAdded: vehicle.timeAdded,
+      timeLastEdited: vehicle.timeLastEdited,
+      launches
+    };
+  } catch (e) {
+    const ne = normalizeError(e);
+    throwErrorForFunction(ne.errorCategory, ne.error);
+  }
 }
 
 export function adminLaunchVehicleEdit(
