973a317f190abf7c3e58a4c35f15dd506c7db7bb
Jiawen LIN <z5647814@ad.unsw.edu.au>
2025-11-11 21:46:15 +1100

    feats: implement rout for deployPayload

--- src/routes/v1/admin/payload.ts
+++ src/routes/v1/admin/payload.ts
@@ -1,13 +1,17 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import router from '.';
+import { Router, Request, Response } from 'express';
+import { adminPayloadDeployedList } from '../../../logic/payload';
 
-router.get('./deployedList', (req: Request, res: Response, next: NextFunction) => {
+const router = Router();
+
+router.get('/deployedList', (req: Request, res: Response) => {
   try {
     const controlUserSessionId = req.header('controlUserSessionId');
-    
+    const result = adminPayloadDeployedList(controlUserSessionId);
+    return res.status(200).json(result);
   } catch (e) {
-    return res.status(e.status).json({ error: e.message });
+    const status = e.status ?? 500;
+    return res.status(status).json({ error: e.message });
   }
+});
 
-}) 
-
+export default router;
