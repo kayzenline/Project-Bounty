6b033d7cddce61bf250fb1acba3a2df722b8839c
Jiawen Lin <z5647814@ad.unsw.edu.au>
2025-11-11 12:44:10 +1100

    feat: router of /v1/admin/astronaut/{astronautid}/llmchat

--- src/routes/v1/admin/astronaut.ts
+++ src/routes/v1/admin/astronaut.ts
@@ -7,6 +7,7 @@ import {
   adminAstronautPool,
 } from '../../../logic/astronaut';
 import HTTPError from 'http-errors';
+import { getMessage } from '../../../logic/llm';
 
 const router = Router();
 
@@ -85,5 +86,32 @@ router.delete('/:astronautid', (req: Request, res: Response, next: NextFunction)
     return res.status(e.status).json({ error: e.message });
   }
 });
+
+router.post('/:astronautid/llmchat', (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const astronautId = parseInt(req.params.astronautid);
+    if (isNaN(astronautId)) {
+      throw HTTPError(400, 'astronautid is invalid');
+    }
+
+    const messageReq = req.body.messageRequest;
+    if (typeof messageReq !== 'string' || messageReq.trim().length === 0) {
+      throw HTTPError(400, 'messageRequest must be a non-empty string');
+    }
+
+    const result = getMessage(messageReq);
+    return res.status(200).json(result);
+  } catch(e) {
+    return res.status(e.status).json({ error: e.message });
+  }
+})
+
+router.get('/:astronautid/llmchat', (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const astronautId = 
+  } catch (e) {
+    
+  }
+})
 
 export default router;
