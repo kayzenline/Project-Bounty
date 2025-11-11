ed9183ee1fd87a5b72a025ae81382bca286f8607
Jiawen LIN <z5647814@ad.unsw.edu.au>
2025-11-12 09:05:22 +1100

    fix: function logic clear

--- src/logic/other.ts
+++ src/logic/other.ts
@@ -9,11 +9,17 @@ export function clear() {
   const data = getData();
   data.controlUsers = [];
   data.spaceMissions = [];
-  data.sessions = [];
   data.nextControlUserId = 1;
   data.nextMissionId = 1;
   data.nextAstronautId = 1;
+  data.nextLaunchVehicleId = 1;
+  data.newtLaunchId = 1;
+  data.sessions = [];
   data.astronauts = [];
+  data.launchVehicles = [];
+  data.launches = [];
+  data.payload = [];
+  data.chatHistory = [];
   setData(data);
   return {};
 }
