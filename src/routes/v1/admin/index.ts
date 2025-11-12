b0a1b918ffe32be67e5bae78d01ca5432b2f7f28
Jiawen LIN <z5647814@ad.unsw.edu.au>
2025-11-12 22:01:05 +1100

    updata: index of route

--- src/routes/v1/admin/index.ts
+++ src/routes/v1/admin/index.ts
@@ -5,6 +5,7 @@ import missionRouter from './mission';
 import astronautRouter from './astronaut';
 import launchVehicleRouter from './launchVehicle';
 import payloadRouter from './payload';
+import launchRouter from './launch';
 
 const router = Router();
 
@@ -13,6 +14,7 @@ router.use('/controluser', controlUserRouter);
 router.use('/mission', missionRouter);
 router.use('/astronaut', astronautRouter);
 router.use('/launchvehicle', launchVehicleRouter);
+router.use('/launch', launchRouter);
 router.use('/payload', payloadRouter);
 
 export default router;
